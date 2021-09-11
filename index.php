84ef54ce64579d8f5014194c62f93ec27a51994c
suzyhannah <abulusuzannah1@gmail.com>
2021-09-11 15:40:22 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>Laravel intro blog Page</h1>
+  <p>This is a Blog built for all my thoughts</p> 
+</div>
+  
+<div class="container">
+   <div class="row">
+      <div class="col-sm-2">
+       </div>
+        <div class="col-sm-8">
+        <div class="card" style="width:100%">
+        <img class="card-img-top" src="blog photo jpeg.jpg" alt="Card image" style="width:100%">
+        <div class="card-body">
+        <h4 class="card-title">Abulu Justina</h4>
+       <p class="card-text">I am Abulu Justina. A student of Bowen University, I study Mechatronics Engineering. I have so many passions, I love fixing things, I love experimenting and exploring on things. My greatest fear is putting my effort into something and not getting a deirable result, living life without achieving my goal and not knowing what my goal is.The effort i am putting in is exploring ever potentials I have,trying to figure out whre i stand so that I'll develop myself in those areas by putting down some goals and ensuring I achieve them and above it all not giving up no matter how difficult or unachievable it might seam. At the end of tech programme, I look forward to in becoming a subject master except in programming and the likes.  </p>
+       <a href="#" class="btn btn-primary">See Profile</a>
+      </div>
+     </div>
+     </div>
+    </div class="col-sm-2">
+    </div>
+   </div>
+ </div>
+</body>
+</html>
